de43d673599ff70bb217c1951c36277c05fa4e5b
Florian Ober <florian.ober@mri.bund.de>
2024-01-11 16:43:23 +0100

    updated some gpl headers inside the soruce files

--- SRC/UncertRadio.F90
+++ SRC/UncertRadio.F90
@@ -1,20 +1,20 @@
-!---------------------------------------------------------------------------------------!
-! UncertRadio - Software for calculation of characteristic threshold values             !
-! Copyright (C) 2014 - 2024 Günter Karnisch                                             !
-!                                                                                       !
-! This program is free software: you can redistribute it and/or modify                  !
-! it under the terms of the GNU General Public License as published by                  !
-! the Free Software Foundation, either version 3 of the License, or                     !
-! (at your option) any later version.                                                   !
-!                                                                                       !
-! This program is distributed in the hope that it will be useful,                       !
-! but WITHOUT ANY WARRANTY; without even the implied warranty of                        !
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                          !
-! GNU General Public License for more details.                                          !
-!                                                                                       !
-! You should have received a copy of the GNU General Public License                     !
-! along with this program. If not, see <https://www.gnu.org/licenses/>.                 !
-!---------------------------------------------------------------------------------------!
+!------------------------------------------------------------------------------!
+! UncertRadio - Software for calculation of characteristic threshold values    !
+! Copyright (C) 2014 - 2024 Günter Karnisch                                    !
+!                                                                              !
+! This program is free software: you can redistribute it and/or modify         !
+! it under the terms of the GNU General Public License as published by         !
+! the Free Software Foundation, either version 3 of the License, or            !
+! (at your option) any later version.                                          !
+!                                                                              !
+! This program is distributed in the hope that it will be useful,              !
+! but WITHOUT ANY WARRANTY; without even the implied warranty of               !
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 !
+! GNU General Public License for more details.                                 !
+!                                                                              !
+! You should have received a copy of the GNU General Public License            !
+! along with this program. If not, see <https://www.gnu.org/licenses/>.        !
+!------------------------------------------------------------------------------!
 
 ! contains:
 ! gui_main
