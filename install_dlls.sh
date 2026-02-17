aefc7dd375d92cdb84acdc0fb6359b36e4be4a71
Florian Ober <florian.ober@mri.bund.de>
2026-02-17 17:09:34 +0100

    moved the windows dependencys to the install_dll file

--- install_dlls.sh
+++ install_dlls.sh
@@ -11,6 +11,10 @@ fi
 # find all needed dll's
 ldd "${install_prefix}/bin/UncertRadio.exe" | grep -i $MSYSTEM | cut -d'=' -f1 | sed -e 's/^[ \t]*//' > myddls.txt
 
+# also collect dependencies of the SVG loader
+ldd "${install_prefix}/lib/gdk-pixbuf-2.0/2.10.0/loaders/pixbufloader_svg.dll" \
+    | grep -i "$MSYSTEM" | cut -d'=' -f1 | sed -e 's/^[ \t]*//' >> myddls.txt
+
 # copy those to the bin directory
 echo 'installing dlls to: '$install_prefix'/bin'
 cat myddls.txt | xargs -I % sh -c 'cp -vu $MSYSTEM_PREFIX/bin/% $0/bin' $install_prefix
